ead7d0803f4f32de8cf2c5925c1da8f2aaaf5b58
Зимин Николай <Nikola.Zimin@x5.ru>
2022-10-07 15:24:22 +0500

    add worker

---
+++ README.md
@@ -0,0 +1,79 @@
+# otus-server
+
+#### Приложение написано в качестве учебного проекта.
+Веб-сервер на сокетах частично реализующий протокол HTTP.
+Реализован кастомный событийный цикл для асинхронной обработки запросов. Готовность сокетов проверяется стандартной библиотекой select.
+
+### Стек
+Для запуска не требуется устанавливать никакие сторонние библиотеки.
+- python 3.10
+
+
+### Запуск
+``` python httpd.py ```
+После чего можно делать запросы к серверу 
+``` http://localhost:port/ ```
+
+### Конфигурация
+Приложению можно передать аргументы первоначальной конфигурации. 
+```
+-r  - корневая директория веб сервера
+-p  - порт на котором будет поднят сервер
+-w  - число воркеров
+
+python httpd.py -r templates -p 5000 -w 2
+```
+
+### Разработка
+Для разработки и тестирования потребуется установить зависимости в ваше окружение.
+``` 
+pip install poetry 
+poetry install
+```
+
+### Запуск тестов, линтеров
+``` 
+python httptest.py
+flake8
+mypy .
+```
+
+### Результаты нагрузочного тестирования
+``` 
+Server Software:        otus-server
+Server Hostname:        127.0.0.1
+Server Port:            6002
+
+Document Path:          /
+Document Length:        104 bytes
+
+Concurrency Level:      100
+Time taken for tests:   34.327 seconds
+Complete requests:      20000
+Failed requests:        0
+Total transferred:      5020000 bytes
+HTML transferred:       2080000 bytes
+Requests per second:    582.64 [#/sec] (mean)
+Time per request:       171.633 [ms] (mean)
+Time per request:       1.716 [ms] (mean, across all concurrent requests)
+Transfer rate:          142.81 [Kbytes/sec] received
+
+Connection Times (ms)
+              min  mean[+/-sd] median   max
+Connect:        1   27 369.4      3    6777
+Processing:     0   46  24.2     51     267
+Waiting:        0   45  24.3     51     267
+Total:          1   73 367.9     59    6781
+
+Percentage of the requests served within a certain time (ms)
+  50%     59
+  66%     61
+  75%     65
+  80%     66
+  90%     73
+  95%     77
+  98%     83
+  99%     99
+ 100%   6781 (longest request)
+
+```
--- httpd.py
+++ httpd.py
@@ -1,11 +1,12 @@
+import os
 import socket
-from select import select
-from wsgiref.handlers import format_date_time
 from datetime import datetime
+from optparse import OptionParser
+from select import select
+from threading import Lock, Thread
 from time import mktime
 from urllib.parse import unquote
-from optparse import OptionParser
-import os
+from wsgiref.handlers import format_date_time
 
 
 TO_MONITOR = []
@@ -24,6 +25,7 @@ CONTENT_TYPES = {
 DEFAULT_CONTENT_TYPE = 'text/plain'
 DEFAULT_FILENAME = 'index.html'
 DEFAULT_EXT = 'html'
+LOCK = Lock()
 
 
 def parse_request(request):
@@ -93,6 +95,18 @@ def get_file(file_path):
         return template.read()
 
 
+def get_file_path_and_ext(root_dir, url):
+    file_path, filename = get_filename_from_path(root_dir + url)
+    filename = get_clear_filename(filename)
+    ext = get_file_ext(filename)
+    if ext is None:
+        filename = DEFAULT_FILENAME
+        ext = DEFAULT_EXT
+
+    file_path = os.path.join(file_path, filename)
+    return file_path, ext
+
+
 def generate_content(code, file_path, ext):
     if code == 404:
         return '<h1>404</h1><p>Not found</p>'
@@ -106,14 +120,7 @@ def generate_content(code, file_path, ext):
 
 def generate_response(request, root_dir):
     method, url = parse_request(request)
-    file_path, filename = get_filename_from_path(root_dir + url)
-    filename = get_clear_filename(filename)
-    ext = get_file_ext(filename)
-    if ext is None:
-        filename = DEFAULT_FILENAME
-        ext = DEFAULT_EXT
-
-    file_path = os.path.join(file_path, filename)
+    file_path, ext = get_file_path_and_ext(root_dir, url)
     headers, code = generate_headers(method, url, file_path)
     body = generate_content(code, file_path, ext)
     headers += generate_header_for_ok(body, ext)
@@ -142,7 +149,8 @@ def send_message(client_socket, root_dir):
 
 def event_loop(server_socket, root_dir):
     while True:
-        ready_to_read, _, _ = select(TO_MONITOR, [], [])
+        with LOCK:
+            ready_to_read, _, _ = select(TO_MONITOR, [], [])
         for sock in ready_to_read:
             if sock is server_socket:
                 accept_connection(sock)
@@ -150,19 +158,25 @@ def event_loop(server_socket, root_dir):
                 send_message(sock, root_dir)
 
 
+def run(server_socket, root_dir):
+    event_loop(server_socket, root_dir)
+
+
 if __name__ == '__main__':
     op = OptionParser()
     op.add_option("-p", "--port", action="store", type=int, default=80)
     op.add_option("-r", "--root_dir", action="store", default='templates')
+    op.add_option("-w", "--workers", action="store", type=int, default=1)
     (opts, args) = op.parse_args()
 
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
     server_socket.bind(('localhost', opts.port))
     server_socket.listen()
 
     TO_MONITOR.append(server_socket)
     try:
-        event_loop(server_socket, opts.root_dir)
+        [Thread(target=run, args=(server_socket, opts.root_dir)).run() for _ in range(opts.workers)]  # type: ignore
     except KeyboardInterrupt:
         pass
